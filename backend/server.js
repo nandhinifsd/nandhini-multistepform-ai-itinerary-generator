1943a322101449171229309a51e6b7195c590146
Nandhini <coolnaandhi@gmail.com>
2026-08-07 18:21:43 +0530

    Refactor server.js to include Firebase integration

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const db = require("./firebase");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
